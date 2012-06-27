b3ad5ea6f7c3ccfed5c9587384a27819ee34f15a
Vincent Paca <vincent.paca@gmail.com>
2012-06-27 18:24:05 +0800

    Refactored code.

--- whosthis.rb
+++ whosthis.rb
@@ -5,12 +5,19 @@ require 'whois'
 require 'anemone'
 
 class WhosThis
-
+  
+  @@filtered_mails = ['domaindiscreet', 'domainsbyproxy', 'whois', 'domains']
+  @@regex = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+  
   def initialize
+    #get inputs
     puts "Input the tags to search for separated in spaces : "
     @tags = gets.chomp
     puts "How many results would you like? Give a number : "
     @pages = gets.chomp
+
+    #init whois
+    @whois = Whois::Client.new
   end
   
   def start
@@ -21,17 +28,21 @@ class WhosThis
 
   def parse(url)
     result = Nokogiri::HTML(open(url))
-    whois = Whois::Client.new
-    r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+    sites = []
+    
     File.open("output.txt", 'w') do |f|
       result.css('h3 a').each do |link|
         begin
           host = URI.parse(link['href'].clean).host
-          who = whois.query(host.gsub('www.', '')).to_s
+          sites << host
+          who = @whois.query(host.gsub('www.', '')).to_s
+          
           puts "Checking WhoIs for #{host}"
-          emails = who.scan(r).uniq
-          emails.each { |email| f.puts "WhoIs: #{email} - #{host}" unless email.include?("domaindiscreet") || email.include?("domainsbyproxy") }
-          if emails.nil?
+          emails = who.scan(@@regex).uniq
+          emails.delete_if { |email| @@filtered_mails.each { |f| email.include?(f) } }
+          emails.each { |email| f.puts "WhoIs: #{email} - #{host}" }
+          
+          if emails.empty?
             begin
               Anemone.crawl("http://#{host}") do |website|
                 checked_urls = []
@@ -39,7 +50,7 @@ class WhosThis
                   unless checked_urls.include?(page.url)
                     puts "Checking #{page.url}..."
                     checked_urls << page.url
-                    emails = "#{page.doc.at('body')}".scan(r).uniq
+                    emails = "#{page.doc.at('body')}".scan(@@regex).uniq
                     emails.each { |email| f.puts "Contact page: #{email} - #{host}" } unless emails.nil?
                     break if checked_urls.count > 5
                   else
