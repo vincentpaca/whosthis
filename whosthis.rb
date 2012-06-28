69b7a91ec6fb01e8685a18bc9aa244098e0ad87b
Vincent Paca <vincent.paca@gmail.com>
2012-06-28 09:09:47 +0800

    Refactored code

--- whosthis.rb
+++ whosthis.rb
@@ -34,28 +34,29 @@ class WhosThis
       result.css('h3 a').each do |link|
         begin
           host = URI.parse(link['href'].clean).host
+          #somtimes, a website gets two spots on the list. greedy bastards.
+          break if sites.include?(host)
           sites << host
           who = @whois.query(host.gsub('www.', '')).to_s
-          
           puts "Checking WhoIs for #{host}"
           emails = who.scan(@@regex).uniq
+          #remove emails which we think are 'host-generated' emails
           emails.delete_if { |email| @@filtered_mails.each { |f| email.include?(f) } }
           emails.each { |email| f.puts "WhoIs: #{email} - #{host}" }
-          
+          #if can't find anything from WhoIs, dig into their contact/about pages
           if emails.empty?
             begin
               Anemone.crawl("http://#{host}") do |website|
                 checked_urls = []
                 website.on_pages_like(/(about|info|contact)/) do |page|
-                  unless checked_urls.include?(page.url)
-                    puts "Checking #{page.url}..."
-                    checked_urls << page.url
-                    emails = "#{page.doc.at('body')}".scan(@@regex).uniq
-                    emails.each { |email| f.puts "Contact page: #{email} - #{host}" } unless emails.nil?
-                    break if checked_urls.count > 5
-                  else
-                    puts "Skipping #{page.url}..."
-                  end
+                  #skip this url if we've been here before
+                  break if checked_urls.include(page.url)
+                  puts "Checking #{page.url}..."
+                  checked_urls << page.url
+                  emails = "#{page.doc.at('body')}".scan(@@regex).uniq
+                  emails.each { |email| f.puts "Contact page: #{email} - #{page.url}" } unless emails.nil?
+                  #NOBODY HAS 5 CONTACT US PAGES. NOBODY. 
+                  break if checked_urls.count > 5
                 end
               end
             rescue Timeout::Error
